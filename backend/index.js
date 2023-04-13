2e64d8e1b041dcc7cc6cf4900ab96f297f530b5c
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-14 02:51:10 +0300

    added dotenv node package

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,8 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.listen(process.env.PORT, (err) => {
+    if (err) return err;
+    console.log("sever is running on port", process.env.PORT);
+});
