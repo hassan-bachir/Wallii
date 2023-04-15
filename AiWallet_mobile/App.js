ca9007194e09b82995b775598e136982c4ccbae0
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-16 00:39:25 +0300

    added necessary libraries

--- AiWallet_mobile/App.js
+++ AiWallet_mobile/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+    console.log("sup");
     return (
         <View style={styles.container}>
             <View style={styles.hassan}>
