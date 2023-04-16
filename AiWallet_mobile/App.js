833faa90360990b4493b297d30f7f9fc2fd5b30c
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-17 00:17:53 +0300

    deleted console log

--- AiWallet_mobile/App.js
+++ AiWallet_mobile/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-    console.log("sup");
     return (
         <View style={styles.container}>
             <View style={styles.hassan}>
