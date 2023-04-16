ba54830c5eb6c183b6b5426e8872c29dba303003
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-17 02:40:17 +0300

    added homepage elements

---
+++ AiWallet_mobile/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, text } from "react-native";
+
+export default function Home() {
+    retrun(
+        <View style={StyleSheet.container}>
+            <Text></Text>
+        </View>
+    );
+}
