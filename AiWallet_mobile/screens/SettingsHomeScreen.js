a0adce038c9e519566badb680426fc33d7fb6c1b
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-17 02:46:43 +0300

    added main components to new screens

---
+++ AiWallet_mobile/screens/SettingsHomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, text } from "react-native";
+
+export default function Home() {
+    retrun(
+        <View style={StyleSheet.container}>
+            <Text></Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+});
