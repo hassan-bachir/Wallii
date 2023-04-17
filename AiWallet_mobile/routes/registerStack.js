2845cceaa5773ea7046b7375240ef3484c9ec80d
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-18 01:55:09 +0300

    changed navigation library

--- AiWallet_mobile/routes/registerStack.js
+++ AiWallet_mobile/routes/registerStack.js
@@ -1,23 +1,42 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/native";
-import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
 import Welcome from "../screens/Register/WelcomeScreen1";
 import ChooseAdvisor from "../screens/Register/ChooseAdvisorScreen2";
 import ChooseName from "../screens/Register/ChooseNameScreen3";
 import ChooseEmail from "../screens/Register/ChooseEmailScreen4";
 
-const Stack = createStackNavigator();
+const screens = {
+    Welcome: {
+        screen: Welcome,
+    },
+    ChooseAdvisor: {
+        screen: ChooseAdvisor,
+    },
+    ChooseName: {
+        screen: ChooseName,
+    },
+    ChooseEmail: {
+        screen: ChooseEmail,
+    },
+};
+const RegisterStack = createStackNavigator(screens);
 
-function RegisterStack() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Welcome" component={Welcome} />
-                <Stack.Screen name="ChooseAdvisor" component={ChooseAdvisor} />
-                <Stack.Screen name="ChooseName" component={ChooseName} />
-                <Stack.Screen name="ChooseEmail" component={ChooseEmail} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-}
-export default RegisterStack;
+export default createAppContainer(RegisterStack);
+
+// const Stack = createStackNavigator();
+
+// function RegisterStack() {
+//     return (
+//         <NavigationContainer>
+//             <Stack.Navigator>
+//                 <Stack.Screen name="Welcome" component={Welcome} />
+//                 <Stack.Screen name="ChooseAdvisor" component={ChooseAdvisor} />
+//                 <Stack.Screen name="ChooseName" component={ChooseName} />
+//                 <Stack.Screen name="ChooseEmail" component={ChooseEmail} />
+//             </Stack.Navigator>
+//         </NavigationContainer>
+//     );
+// }
+// export default RegisterStack;
