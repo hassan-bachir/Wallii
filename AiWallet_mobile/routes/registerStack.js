46ecd288c5b78b7ea7ceb51e8b56328804d51b30
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-18 00:43:44 +0300

    added navigation container

--- AiWallet_mobile/routes/registerStack.js
+++ AiWallet_mobile/routes/registerStack.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import Welcome from "../screens/Register/WelcomeScreen1";
 import ChooseAdvisor from "../screens/Register/ChooseAdvisorScreen2";
 import ChooseName from "../screens/Register/ChooseNameScreen3";
