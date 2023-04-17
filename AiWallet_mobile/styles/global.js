260417faded8cccca9b4d812651958b895905ded
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-17 22:50:32 +0300

    added global styles file

---
+++ AiWallet_mobile/styles/global.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    titleText: {
+        fontSize: 18,
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20,
+    },
+});
