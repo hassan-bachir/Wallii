bfc0ec8a953bafb1e35aa8c35458c33d9209c1ff
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-18 02:18:15 +0300

    added navigation button

--- AiWallet_mobile/screens/Register/WelcomeScreen1.js
+++ AiWallet_mobile/screens/Register/WelcomeScreen1.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../../styles/global";
 
-export default function Welcome() {
+export default function Welcome({ navigation }) {
+    const pressHandler = () => {
+        navigation.push("ChooseAdvisor");
+    };
+
     return (
         <View style={globalStyles.container}>
             <Text>Welcome Screen</Text>
+            <Button title="Get Started" onPress={pressHandler}></Button>
         </View>
     );
 }
