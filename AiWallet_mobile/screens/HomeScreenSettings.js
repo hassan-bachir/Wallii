75c9555c8fe8a611f279966362fe9885a23f168e
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-17 23:11:39 +0300

    added global styling to screen

--- AiWallet_mobile/screens/HomeScreenSettings.js
+++ AiWallet_mobile/screens/HomeScreenSettings.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function HomeSettings() {
     return (
