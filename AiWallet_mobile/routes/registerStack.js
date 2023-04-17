ceb778634f6cd9baf2e494ab5ba146a3ba7d1f76
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-18 00:43:18 +0300

    add register navigation stack

---
+++ AiWallet_mobile/routes/registerStack.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/native";
+import Welcome from "../screens/Register/WelcomeScreen1";
+import ChooseAdvisor from "../screens/Register/ChooseAdvisorScreen2";
+import ChooseName from "../screens/Register/ChooseNameScreen3";
+import ChooseEmail from "../screens/Register/ChooseEmailScreen4";
+
+const Stack = createStackNavigator();
