d55db717b0dbff1ebd9950d5a9c0d1bc27a1b5a7
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-17 03:36:39 +0300

    added loadAsync for fonts

--- AiWallet_mobile/App.js
+++ AiWallet_mobile/App.js
@@ -1,5 +1,16 @@
 import React from "react";
 import Home from "./screens/HomeScreen";
+import * as Font from "expo-font";
+
+const getFonts = () =>
+    Font.loadAsync({
+        "Montserrat-medium": require("./assets/fonts/MontserratAlternates-Regular.ttf"),
+        "Montserrat-medium": require("./assets/fonts/MontserratAlternates-Medium.ttf"),
+        "Montserrat-medium": require("./assets/fonts/MontserratAlternates-Italic.ttf"),
+        "Montserrat-medium": require("./assets/fonts/MontserratAlternates-SemiBold.ttf"),
+        "Montserrat-medium": require("./assets/fonts/MontserratAlternates-Bold.ttf"),
+    });
+
 export default function App() {
     return <Home />;
 }
