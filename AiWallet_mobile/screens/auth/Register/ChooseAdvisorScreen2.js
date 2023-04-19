b69fc22d1488d61ab21d8e380a84ad2ab665a6f8
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 00:42:47 +0300

    added default tage to screens

--- AiWallet_mobile/screens/auth/Register/ChooseAdvisorScreen2.js
+++ AiWallet_mobile/screens/auth/Register/ChooseAdvisorScreen2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
-import { globalStyles } from "../../styles/global";
+import { globalStyles } from "../../../styles/global";
 
 export default function ChooseAdvisor({ navigation }) {
     const pressHandler = () => {
