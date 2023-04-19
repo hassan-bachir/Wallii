11416b886eb3dcfbad9bdc3f92b1bfa84dea3abe
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 00:29:48 +0300

    added routes constant

---
+++ AiWallet_mobile/constants/routes.js
@@ -0,0 +1,10 @@
+export default {
+    LOGIN: "Login",
+    REGISTER: "Register",
+
+    HOME: "Home",
+    HOME_SETTINGS: "Home Settings",
+
+    WALLET: "Wallet",
+    WALLET_SETTINGS: "Wallet Settings",
+};
