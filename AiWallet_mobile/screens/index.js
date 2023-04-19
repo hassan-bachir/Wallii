f0477667f979561ea333ed4c964ab7d242b477cd
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 01:58:26 +0300

    finalised index.js in screens

--- AiWallet_mobile/screens/index.js
+++ AiWallet_mobile/screens/index.js
@@ -1,5 +1,16 @@
 export { default as Login } from "./auth/LoginScreen";
+
 export { default as Welcome } from "./auth/Register/WelcomeScreen1";
 export { default as ChooseAdvisor } from "./auth/Register/ChooseAdvisorScreen2";
 export { default as ChooseName } from "./auth/Register/ChooseNameScreen3";
 export { default as ChooseEmail } from "./auth/Register/ChooseEmailScreen4";
+
+export { default as Home } from "./home/HomeScreen";
+export { default as HomeSettings } from "./home/HomeSettingsScreen";
+
+export { default as Wallet } from "./wallet/WalletScreen";
+export { default as WalletSettings } from "./wallet/WalletSettingsScreen";
+export { default as AddRecord } from "./wallet/AddRecordScreen";
+export { default as UpdateRecord } from "./wallet/UpdateRecordScreen";
+export { default as WalletBudget } from "./wallet/WalletBudgetScreen";
+export { default as WalletStats } from "./wallet/WalletStatsScreen";
