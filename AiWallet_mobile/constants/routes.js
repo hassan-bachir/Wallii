65032b8eabfb195f8b1b8e1d65db9ed8bb86feb3
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 02:01:28 +0300

    updated routes constant

--- AiWallet_mobile/constants/routes.js
+++ AiWallet_mobile/constants/routes.js
@@ -1,5 +1,6 @@
 export default {
     LOGIN: "Login",
+
     WELCOME: "Welcome",
     CHOOSE_ADVISOR: "Choose Advisor",
     CHOOSE_NAME: "Choose name",
@@ -10,6 +11,8 @@ export default {
 
     WALLET: "Wallet",
     WALLET_SETTINGS: "Wallet Settings",
+    WALLET_ADD_RECORD: "Add Record",
+    WALLET_UPDATE_RECORD: "Update Record",
     WALLET_STATS: "Wallet Stats",
     WALLET_BUDGET: "Wallet Budget",
 };
