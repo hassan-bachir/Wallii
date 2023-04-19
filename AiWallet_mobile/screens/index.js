6cbffbe60fba670e746cbab35ab8d1423b5fa1bb
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 01:52:21 +0300

    added exports to index

---
+++ AiWallet_mobile/screens/index.js
@@ -0,0 +1,5 @@
+export { default as Login } from "./auth/LoginScreen";
+export { default as Welcome } from "./auth/Register/WelcomeScreen1";
+export { default as ChooseAdvisor } from "./auth/Register/ChooseAdvisorScreen2";
+export { default as ChooseName } from "./auth/Register/ChooseNameScreen3";
+export { default as ChooseEmail } from "./auth/Register/ChooseEmailScreen4";
