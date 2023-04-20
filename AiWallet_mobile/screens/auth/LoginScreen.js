59bcc621b24cff73b7d6d83f6857ff41a6f1a555
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 23:54:51 +0300

    added back navigation to login

--- AiWallet_mobile/screens/auth/LoginScreen.js
+++ AiWallet_mobile/screens/auth/LoginScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { globalStyles } from "../../styles/global";
+import { ROUTES } from "../../constants";
 
-export default function Login() {
+export default function Login({ navigation }) {
+    const navigateToWelcome = () => {
+        navigation.navigate(ROUTES.WELCOME);
+    };
+
     return (
         <View style={globalStyles.container}>
             <Text>Login Screen</Text>
+            <Button title="Get Started" onPress={navigateToWelcome} />
         </View>
     );
 }
