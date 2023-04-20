5c699f150eb76ffffc82ee362a5ba5513f17eddb
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 23:58:04 +0300

    added routes to the auth screens

--- AiWallet_mobile/screens/auth/Register/ChooseAdvisorScreen2.js
+++ AiWallet_mobile/screens/auth/Register/ChooseAdvisorScreen2.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../../../styles/global";
+import { ROUTES } from "../../../constants";
 
 export default function ChooseAdvisor({ navigation }) {
     const pressHandler = () => {
-        navigation.push("ChooseName");
+        navigation.push(ROUTES.CHOOSE_NAME);
     };
     return (
         <View style={globalStyles.container}>
             <Text>Choose Advisor Screen</Text>
-            <Button title="Next" onPress={pressHandler}></Button>
+            <Button title="Next" onPress={pressHandler} />
         </View>
     );
 }
--- AiWallet_mobile/screens/auth/Register/ChooseNameScreen3.js
+++ AiWallet_mobile/screens/auth/Register/ChooseNameScreen3.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../../../styles/global";
+import { ROUTES } from "../../../constants";
 
 export default function ChooseName({ navigation }) {
     const pressHandler = () => {
