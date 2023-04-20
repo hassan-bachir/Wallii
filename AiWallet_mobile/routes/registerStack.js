c97d6e71cbe888dfab5c0bb40cccacf573e6285e
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-20 23:35:09 +0300

    added export

--- AiWallet_mobile/routes/registerStack.js
+++ AiWallet_mobile/routes/registerStack.js
@@ -7,15 +7,22 @@ import {
     ChooseEmail,
 } from "../screens";
 import { ROUTES } from "../constants/";
+
 const Stack = createStackNavigator();
 
-function MyStack() {
+function AuthStack() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Notifications" component={Notifications} />
-            <Stack.Screen name="Profile" component={Profile} />
-            <Stack.Screen name="Settings" component={Settings} />
+            <Stack.Screen name={ROUTES.WELCOME} component={Welcome} />
+            <Stack.Screen name={ROUTES.LOGIN} component={Login} />
+            <Stack.Screen
+                name={ROUTES.CHOOSE_ADVISOR}
+                component={ChooseAdvisor}
+            />
+            <Stack.Screen name={ROUTES.CHOOSE_NAME} component={ChooseName} />
+            <Stack.Screen name={ROUTES.CHOOSE_EMAIL} component={ChooseEmail} />
         </Stack.Navigator>
     );
 }
+
+export default AuthStack;
