ca2e24c84efcd54b9615a63444e066c6f46bc2ea
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-21 05:18:24 +0300

    fixed errors in register API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const generateToken = (user) => {
     return jwt.sign(
         { id: user._id, email: user.email },
-        process.env.JWT_SECRET,
+        process.env.SECRET_KEY,
         { expiresIn: "30d" }
     );
 };
