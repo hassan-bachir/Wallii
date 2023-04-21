fc9e4267d8f4e4c86b923d03568fabfda3873f3e
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-21 03:26:22 +0300

    added register function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,2 +1,20 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+    const { email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+        return res.status(409).json({ message: "Email already exists" });
+
+    const user = new User();
+    user.email = email;
+    user.password = password;
+
+    if (role) user.role = role;
+    await user.save();
+    const { password: hashedPassword, ...newUser } = user.toJSON();
+    res.status(201).json(newUser);
+};
