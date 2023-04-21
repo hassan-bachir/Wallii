14ae98b4436fd2353aeaac68282d70bca9c8325e
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-21 03:58:39 +0300

    updated the register function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,22 +10,37 @@ const generateToken = (user) => {
     );
 };
 
-exports.register = async (req, res) => {
-    const { email, password, role } = req.body;
+const register = async (req, res) => {
+    try {
+        const { name, lastName, email, password, aiAdvisorName } = req.body;
 
-    const existingUser = await User.findOne({ email });
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
 
-    if (existingUser)
-        return res.status(409).json({ message: "Email already exists" });
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new User();
-    user.email = email;
-    user.password = password;
+        const newUser = new User({
+            name,
+            lastName,
+            email,
+            password: hashedPassword,
+            aiAdvisorName,
+        });
 
-    if (role) user.role = role;
-    await user.save();
-    const { password: hashedPassword, ...newUser } = user.toJSON();
-    res.status(201).json(newUser);
+        await newUser.save();
+
+        const token = generateToken(newUser);
+
+        res.status(201).json({
+            message: "User registered successfully",
+            newUser,
+            token,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
 };
 
 exports.login = async (req, res) => {
