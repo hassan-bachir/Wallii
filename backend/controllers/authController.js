a2b85daa8835f7ff5f5339d227588204e009cc25
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-22 01:56:18 +0300

    updated constant name

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const generateToken = (user) => {
     return jwt.sign(
         { id: user._id, email: user.email },
-        process.env.SECRET_KEY,
+        process.env.JWT_SECRET,
         { expiresIn: "30d" }
     );
 };
