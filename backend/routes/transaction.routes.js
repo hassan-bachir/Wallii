faa1db1fe92c09609e0ebcba9b30fabf1f11e6a3
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-23 00:39:56 +0300

    added middleware require

--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const transactionController = require("../controllers/transactionController");
+const {
+    authMiddleware: checkToken,
+} = require("../middlewares/auth.middleware");
