459840fad7e9cfcd149a7b082362a46f2cd3bf9e
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-22 08:04:15 +0300

    added route and comments

--- backend/routes/wallet.routes.js
+++ backend/routes/wallet.routes.js
@@ -5,10 +5,13 @@ const {
     authMiddleware: checkToken,
 } = require("../middlewares/auth.middleware");
 
+//add wallet
 router.post("/wallet", checkToken, walletController.addWallet);
-
+//get all wallets
 router.get("/wallet", checkToken, walletController.getUserWallets);
-
+//update wallet
 router.put("/:walletId", checkToken, walletController.updateWallet);
+//get wallet by ID
+router.get("/:walletId", checkToken, walletController.getWallet);
 
 module.exports = router;
