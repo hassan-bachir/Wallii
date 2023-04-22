22d2b00cc44ccb45388ce30e1bb51e2dff4cf10d
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-23 00:29:01 +0300

    added add transaction function

---
+++ backend/controllers/transactionController.js
@@ -0,0 +1,44 @@
+const Wallet = require("../models/walletModel");
+const Transaction = require("../models/transactionModel");
+
+const addTransaction = async (req, res) => {
+    try {
+        const { walletId } = req.params;
+        const {
+            type,
+            category,
+            amount,
+            date,
+            description,
+            isRecurring,
+            recurringPeriod,
+            receiptImageUrl,
+        } = req.body;
+
+        const newTransaction = new Transaction({
+            walletId,
+            type,
+            category,
+            amount,
+            date,
+            description,
+            isRecurring,
+            recurringPeriod,
+            receiptImageUrl,
+        });
+
+        const savedTransaction = await newTransaction.save();
+
+        await Wallet.findByIdAndUpdate(walletId, {
+            $push: { transactions: savedTransaction._id },
+        });
+
+        res.status(201).json(savedTransaction);
+    } catch (error) {
+        res.status(500).json({ message: "Error adding transaction", error });
+    }
+};
+
+module.exports = {
+    addTransaction,
+};
