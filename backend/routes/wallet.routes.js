85101e4128c552cada4b2d5fb4b4b2034f9c370b
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-22 06:11:47 +0300

    added the route to update wallet

--- backend/routes/wallet.routes.js
+++ backend/routes/wallet.routes.js
@@ -9,4 +9,6 @@ router.post("/wallet", checkToken, walletController.addWallet);
 
 router.get("/wallet", checkToken, walletController.getUserWallets);
 
+router.put("/:walletId", checkToken, walletController.updateWallet);
+
 module.exports = router;
