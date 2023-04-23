4ba51e86f57384f119e19d0f09e767e63f7769eb
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-23 18:32:51 +0300

    adding essential comments

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+// (/auth/register)
 router.post("/register", authController.register);
+// (/auth/login)
 router.post("/login", authController.login);
 
 module.exports = router;
--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -6,28 +6,39 @@ const {
 } = require("../middlewares/auth.middleware");
 
 // Add transaction
+// (/transaction/:walletId) POST
 router.post("/:walletId", checkToken, transactionController.addTransaction);
+
 //get all transactions
+// (/transaction/:walletId)
 router.get("/:walletId", checkToken, transactionController.getAllTransactions);
+
 //get transaction by ID
+// (/transaction/register)
 router.get(
     "/byid/:transactionId",
     checkToken,
     transactionController.getTransactionById
 );
+
 //update transaction
+// (/transaction/register)
 router.put(
     "/:transactionId",
     checkToken,
     transactionController.updateTransaction
 );
+
 //delete transaction
+// (/transaction/register)
 router.delete(
     "/:transactionId",
     checkToken,
     transactionController.deleteTransaction
 );
+
 // Get transactions grouped by date
+// (/transaction/register)
 router.get(
     "/:walletId/by-date",
     checkToken,
