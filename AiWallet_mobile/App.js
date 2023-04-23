3625be44f44b791aa87885d2089965542570f212
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-23 17:06:46 +0300

    added provider to app

--- AiWallet_mobile/App.js
+++ AiWallet_mobile/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { NavigationContainer } from "@react-navigation/native";
-import AuthStack from "./routes/authStack";
 import HomeStack from "./routes/homeStack";
-
+import store from "./store";
+import { Provider } from "react-redux";
 import { loadFonts } from "./constants";
 
 export default function App() {
