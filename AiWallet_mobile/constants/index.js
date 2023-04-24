4fff3c7c06c1e85eccdbc22ba8826adc37727f8d
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-24 08:47:35 +0300

    added images and icons to the index

--- AiWallet_mobile/constants/index.js
+++ AiWallet_mobile/constants/index.js
@@ -1,5 +1,7 @@
 import ROUTES from "./routes";
 import COLORS from "./colors";
 import loadFonts from "./fonts";
+import icons from "./icons";
+import images from "./images";
 
-export { ROUTES, COLORS, loadFonts };
+export { ROUTES, COLORS, loadFonts, images, icons };
