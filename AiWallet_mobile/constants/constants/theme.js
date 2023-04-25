34125382d7d10e1e6bcc1857372e7fcd2ad83a34
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-25 15:04:43 +0300

    mobile.. added the main font sizes

--- AiWallet_mobile/constants/constants/theme.js
+++ AiWallet_mobile/constants/constants/theme.js
@@ -30,3 +30,28 @@ export const COLORS = {
 
     transparent: "transparent",
 };
+
+export const SIZES = {
+    // global sizes
+    base: 8,
+    font: 14,
+    radius: 30,
+    padding: 10,
+    padding2: 12,
+
+    // font sizes
+    largeTitle: 50,
+    h1: 30,
+    h2: 22,
+    h3: 20,
+    h4: 18,
+    body1: 30,
+    body2: 20,
+    body3: 16,
+    body4: 14,
+    body5: 12,
+
+    // app dimensions
+    width,
+    height,
+};
