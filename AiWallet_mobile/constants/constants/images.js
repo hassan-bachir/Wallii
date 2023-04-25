e9a74ed3fc9a3f5b24eeff1a5ecca967c7b9006d
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-25 22:17:37 +0300

    deleted non wanted images

--- AiWallet_mobile/constants/constants/images.js
+++ AiWallet_mobile/constants/constants/images.js
@@ -1,9 +1,7 @@
 const Logo = require("../../assets/images/logo.png");
 const background = require("../../assets/images/background5.png");
-const bill = require("../../assets/images/bill.png");
 
 export default {
     Logo,
     background,
-    bill,
 };
