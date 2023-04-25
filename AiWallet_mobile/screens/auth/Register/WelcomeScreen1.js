386bcb6fd7e9e308d80dcac641e2e7d4e77f1a5f
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-25 20:48:08 +0300

    added slogan to welcome page

--- AiWallet_mobile/screens/auth/Register/WelcomeScreen1.js
+++ AiWallet_mobile/screens/auth/Register/WelcomeScreen1.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { globalStyles } from "../../../styles/global";
-import { ROUTES } from "../../../constants";
+import { ROUTES, FONTS, COLORS } from "../../../constants";
 import { Background, Button, Logo } from "../../../components";
 
 export default function Welcome({ navigation }) {
@@ -16,9 +16,22 @@ export default function Welcome({ navigation }) {
         <Background>
             <View style={globalStyles.container}>
                 <Logo />
+                <Text style={styles.slogan}>
+                    Money Management Made Simple: AI's Revolutionary Approach to
+                    Finances!
+                </Text>
                 <Button title="GET STARTED" onPress={navigateToChooseAdvisor} />
                 <Button title="LOGIN" onPress={navigateToLogin} />
             </View>
         </Background>
     );
 }
+const styles = StyleSheet.create({
+    slogan: {
+        ...FONTS.h3,
+        color: COLORS.white,
+        textAlign: "center",
+        paddingHorizontal: 20,
+        marginBottom: 20,
+    },
+});
