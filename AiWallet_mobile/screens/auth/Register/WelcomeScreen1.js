316d6278c5d5dcdda0e1b93edc3d352599c72a2b
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-25 22:01:39 +0300

    mobile..updated welcome page's layout

--- AiWallet_mobile/screens/auth/Register/WelcomeScreen1.js
+++ AiWallet_mobile/screens/auth/Register/WelcomeScreen1.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import { globalStyles } from "../../../styles/global";
 import { ROUTES, FONTS, COLORS } from "../../../constants";
 import { Background, Button, Logo } from "../../../components";
@@ -14,21 +14,52 @@ export default function Welcome({ navigation }) {
     };
     return (
         <Background>
-            <View style={globalStyles.container}>
-                <Logo />
-                <Text style={styles.slogan}>
-                    Money Management Made Simple: AI's Revolutionary Approach to
-                    Finances!
-                </Text>
-                <Button title="GET STARTED" onPress={navigateToChooseAdvisor} />
-                <Text style={styles.loginLink} onPress={navigateToLogin}>
-                    Already a user? Login
-                </Text>
-            </View>
+            <SafeAreaView style={styles.safeArea}>
+                <View style={globalStyles.container}>
+                    <View style={styles.logoContainer}>
+                        <Logo />
+                    </View>
+                    <View style={styles.sloganContainer}>
+                        <Text style={styles.slogan}>
+                            Money Management Made Simple: AI's Revolutionary
+                            Approach to Finances! 👋
+                        </Text>
+                    </View>
+                    <View style={styles.buttonsContainer}>
+                        <Button
+                            title="GET STARTED"
+                            onPress={navigateToChooseAdvisor}
+                        />
+                        <Text
+                            style={styles.loginLink}
+                            onPress={navigateToLogin}
+                        >
+                            Already a user? Login
+                        </Text>
+                    </View>
+                </View>
+            </SafeAreaView>
         </Background>
     );
 }
 const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+    logoContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    sloganContainer: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    buttonsContainer: {
+        flex: 1,
+        justifyContent: "center",
+        marginBottom: 20,
+    },
     slogan: {
         ...FONTS.h3,
         color: COLORS.white,
