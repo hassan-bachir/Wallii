f9919a5250303860d8a191f2524e0e41f9e571f6
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-28 00:32:28 +0300

    mobile..changed naming of constants

--- AiWallet_mobile/screens/auth/Register/ChooseAdvisorScreen2.js
+++ AiWallet_mobile/screens/auth/Register/ChooseAdvisorScreen2.js
@@ -7,10 +7,15 @@ import {
     Text,
 } from "react-native";
 import { globalStyles } from "../../../styles/global";
-import { ROUTES, FONTS, COLORS } from "../../../constants";
+import { ROUTES, FONTS, COLORS, images } from "../../../constants";
 import { Background, Button } from "../../../components";
 import { useDispatch } from "react-redux";
 import { setSelectedAdvisor } from "../../../store/slices/registrationSlice";
+
+const advisors = [
+    { id: 1, name: "Advisor 1", image: "https://example.com/advisor1.jpg" },
+    { id: 2, name: "Advisor 2", image: "https://example.com/advisor2.jpg" },
+];
 
 export default function ChooseAdvisor({ navigation }) {
     const dispatch = useDispatch();
