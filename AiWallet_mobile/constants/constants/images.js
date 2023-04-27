43c58186f0f057670131b3c0d0459d3f18dcd8fb
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-28 00:04:55 +0300

    mobile..added defaultimages

--- AiWallet_mobile/constants/constants/images.js
+++ AiWallet_mobile/constants/constants/images.js
@@ -1,11 +1,5 @@
 const Logo = require("../../assets/images/logo.png");
 const background = require("../../assets/images/background5.png");
-const background = require("../../assets/images/background5.png");
-const background = require("../../assets/images/background5.png");
-const background = require("../../assets/images/background5.png");
-const background = require("../../assets/images/background5.png");
-const background = require("../../assets/images/background5.png");
-const background = require("../../assets/images/background5.png");
 
 export default {
     Logo,
