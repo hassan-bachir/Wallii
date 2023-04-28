415bbb74f0bd4fdcdf19df8a279876f7c19aa537
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-28 18:30:52 +0300

    mobile..added styling sheets to register

--- AiWallet_mobile/screens/auth/Register/Register.js
+++ AiWallet_mobile/screens/auth/Register/Register.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, SafeAreaView, StyleSheet } from "react-native";
 
-import { ROUTES } from "../../../constants";
+import { ROUTES, FONTS, COLORS } from "../../../constants";
 import { Background, Button } from "../../../components";
 
 export default function Register({ navigation }) {
+    const navigateBackToChooseAdvisor = () => {
+        navigation.goBack();
+    };
     return (
         <Background>
             <SafeAreaView style={styles.container}>
@@ -16,5 +19,17 @@ export default function Register({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    buttonsContainer: {
+        justifyContent: "center",
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    goBackLink: {
+        ...FONTS.body3,
+        color: COLORS.white,
+        textAlign: "center",
+        textDecorationLine: "underline",
+        marginTop: 10,
     },
 });
