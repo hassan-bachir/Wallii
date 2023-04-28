23661de34cf9a60074124fabda5cb5464b63d580
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-28 13:17:35 +0300

    mobile..updated the advisor card

--- AiWallet_mobile/components/AdvisorCard/AdvisorCard.js
+++ AiWallet_mobile/components/AdvisorCard/AdvisorCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { COLORS, FONTS, SIZES } from "../../constants";
+import { Dimensions } from "react-native";
+const { width } = Dimensions.get("window");
+
+const scaleFactor = width / 414; // 414 is the reference width (iPhone 11 Pro Max)
 
 const AdvisorCard = ({ advisor, onPress }) => {
     const { name, image } = advisor;
@@ -14,17 +19,20 @@ const AdvisorCard = ({ advisor, onPress }) => {
         </TouchableOpacity>
     );
 };
-
 const styles = StyleSheet.create({
     card: {
         alignItems: "center",
         justifyContent: "center",
         margin: 10,
+        width: width * 0.4 * scaleFactor, // Adjust the width based on scaleFactor
+        backgroundColor: COLORS.primary,
+        borderRadius: 10,
+        padding: 10,
     },
     imageContainer: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
+        width: 80 * scaleFactor, // Adjust the width based on scaleFactor
+        height: 80 * scaleFactor, // Adjust the height based on scaleFactor
+        borderRadius: 40 * scaleFactor, // Adjust the borderRadius based on scaleFactor
         overflow: "hidden",
         marginBottom: 10,
     },
@@ -33,8 +41,11 @@ const styles = StyleSheet.create({
         height: "100%",
     },
     name: {
-        fontSize: 16,
+        fontSize: 16 * scaleFactor, // Adjust the fontSize based on scaleFactor
         fontWeight: "bold",
+        color: COLORS.white,
+        fontFamily: FONTS.body3.fontFamily,
     },
 });
+
 export default AdvisorCard;
