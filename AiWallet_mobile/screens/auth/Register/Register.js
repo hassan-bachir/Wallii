2ec32c6980ba0e48ebec857717702f5dd211cd66
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-28 18:24:49 +0300

    mobile..added background and fixed button in register page

--- AiWallet_mobile/screens/auth/Register/Register.js
+++ AiWallet_mobile/screens/auth/Register/Register.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, SafeAreaView } from "react-native";
 import { globalStyles } from "../../../styles/global";
 import { ROUTES } from "../../../constants";
+import { Background, Button } from "../../../components";
 
 export default function Register({ navigation }) {
     return (
-        <View style={globalStyles.container}>
-            <Text>Register</Text>
-            <Button title="Next"></Button>
-        </View>
+        <Background>
+            <View style={globalStyles.container}>
+                <Button title="Done"></Button>
+            </View>
+        </Background>
     );
 }
