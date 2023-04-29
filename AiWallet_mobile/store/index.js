178cdd589144496cca6a188010573b251d4332f0
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-29 21:51:58 +0300

    mobile..added the login slice

--- AiWallet_mobile/store/index.js
+++ AiWallet_mobile/store/index.js
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
+
 import walletReducer from "./slices/walletSlice";
 import transactionReducer from "./slices/transactionSlice";
 import registrationReducer from "./slices/registrationSlice";
 
 const store = configureStore({
     reducer: {
-        auth: authReducer,
         wallet: walletReducer,
         transaction: transactionReducer,
         registration: registrationReducer,
