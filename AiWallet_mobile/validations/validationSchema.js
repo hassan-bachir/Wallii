b725ddda0408e32c98cb21186badcf64ee5fa5e1
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-29 22:19:05 +0300

    mobile..added yup login validation schema

--- AiWallet_mobile/validations/validationSchema.js
+++ AiWallet_mobile/validations/validationSchema.js
@@ -15,3 +15,10 @@ export const registerValidationSchema = Yup.object().shape({
         .min(8, "Password should be at least 8 characters")
         .required("Password is required"),
 });
+
+export const loginValidationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string()
+        .min(8, "Password must be at least 8 characters")
+        .required("Password is required"),
+});
