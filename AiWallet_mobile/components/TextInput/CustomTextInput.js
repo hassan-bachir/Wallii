bfb7d9dfcf879675255a870e904b517739e8739a
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-29 12:49:36 +0300

    mobile..styled the text input

--- AiWallet_mobile/components/TextInput/CustomTextInput.js
+++ AiWallet_mobile/components/TextInput/CustomTextInput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput as RNTextInput, StyleSheet } from "react-native";
 import { COLORS, FONTS, SIZES } from "../../constants";
 
@@ -9,16 +9,34 @@ const CustomTextInput = ({
     secureTextEntry = false,
     ...otherProps
 }) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    const handleFocus = () => {
+        setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+    };
     return (
         <View style={styles.container}>
             <Text style={styles.label}>{label}</Text>
             <RNTextInput
-                style={styles.input}
+                style={[
+                    styles.input,
+                    {
+                        borderBottomColor: isFocused
+                            ? COLORS.secondary
+                            : COLORS.white,
+                    },
+                ]}
                 placeholder={placeholder}
                 placeholderTextColor={COLORS.white}
                 selectionColor={COLORS.white}
                 onChangeText={onChangeText}
                 secureTextEntry={secureTextEntry}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
             />
         </View>
     );
