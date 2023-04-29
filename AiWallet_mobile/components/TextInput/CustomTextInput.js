8be0523aba741cdb6bcd8f01fdbe3ad363dd75e8
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-29 13:35:39 +0300

    mobile..imported icons to text input

--- AiWallet_mobile/components/TextInput/CustomTextInput.js
+++ AiWallet_mobile/components/TextInput/CustomTextInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TextInput as RNTextInput, StyleSheet } from "react-native";
-import { COLORS, FONTS, SIZES } from "../../constants";
+import { COLORS, FONTS, SIZES, ICONS } from "../../constants";
 
 const CustomTextInput = ({
     label,
