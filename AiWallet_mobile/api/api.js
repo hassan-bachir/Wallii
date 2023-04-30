40c6a3b9781c3690b352db3273e2cf9e2ea2a53a
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-30 10:31:03 +0300

    mobile..added makeRequest function for API calls

--- AiWallet_mobile/api/api.js
+++ AiWallet_mobile/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "localhost:3000/";
+const API_BASE_URL = "http://localhost:3000";
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
@@ -8,5 +8,25 @@ const apiClient = axios.create({
         "Content-Type": "application/json",
     },
 });
+
+const makeRequest = async (method, endpoint, data) => {
+    try {
+        const response = await apiClient({
+            method: method,
+            url: endpoint,
+            data: data,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(`Error ${method} ${endpoint}:`, error);
+        throw error;
+    }
+};
+
+export const registerUser = (userData) =>
+    makeRequest("post", "/auth/register", userData);
+
+export const loginUser = (credentials) =>
+    makeRequest("post", "/auth/login", credentials);
 
 export default apiClient;
