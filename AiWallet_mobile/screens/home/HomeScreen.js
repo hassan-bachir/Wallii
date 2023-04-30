31aa14abe4237d343ba4eef5a78e02afc303c27e
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-30 17:23:35 +0300

    mobile..added padding to ssafeAreaView

--- AiWallet_mobile/screens/home/HomeScreen.js
+++ AiWallet_mobile/screens/home/HomeScreen.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import { Button, Container } from "../../components";
 import { ROUTES } from "../../constants";
 
 export default function Home({ navigation }) {
-    const navigateToHome = () => {
+    const navigateToHomeSettings = () => {
         navigation.navigate(ROUTES.HOME_SETTINGS);
     };
 
     return (
-        <View>
+        <SafeAreaView style={styles.Safe}>
             <Text>Home Screen</Text>
-            <Button title="settings" onPress={navigateToHome} />
-        </View>
+            <Button title="settings" onPress={navigateToHomeSettings} />
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    Safe: {
+        paddingTop: 20,
+    },
+});
