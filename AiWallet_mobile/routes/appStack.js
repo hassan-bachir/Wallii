3634977155b878d2ae4b4324652c8dff11f1b8a2
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-30 16:13:17 +0300

    mobile..added token validation to the app stack

--- AiWallet_mobile/routes/appStack.js
+++ AiWallet_mobile/routes/appStack.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeStack from "./homeStack";
 import AuthStack from "./authStack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ROUTES } from "../constants";
 
 const Stack = createStackNavigator();
 
 function AppStack() {
+    const [userToken, setUserToken] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
     return (
         <Stack.Navigator
             initialRouteName={ROUTES.AUTH}
