fcbf26cd415c37721f90a03214994c0564966cab
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-04-30 15:54:39 +0300

    mobile..imported Async storage to app

--- AiWallet_mobile/App.js
+++ AiWallet_mobile/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { NavigationContainer } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import AppStack from "./routes/appStack";
 import store from "./store";
 import { Provider } from "react-redux";
