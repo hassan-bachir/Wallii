28dc132aca7d701e496a20a24411d826023d60a9
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-01 16:36:26 +0300

    mobile..added wallet custom api functions

--- AiWallet_mobile/api/api.js
+++ AiWallet_mobile/api/api.js
@@ -41,16 +41,45 @@ const makeRequest = async (method, endpoint, data) => {
 //AUTH
 export const registerUser = (userData) =>
     makeRequest("post", "/auth/register", userData);
+
 export const loginUser = (credentials) =>
     makeRequest("post", "/auth/login", credentials);
 
 //USER
 export const getUserInfo = () => makeRequest("get", "/user");
+
 export const updateUserInfo = (data) => makeRequest("put", "/user", data);
+
 export const addGoal = (data) => makeRequest("post", "/user/goals", data);
+
 export const getFinancialSummary = () =>
     makeRequest("get", "/user/financial-summary");
+
 export const deleteGoal = (goalId) =>
     makeRequest("delete", `/user/goals/${goalId}`);
+
+//WALLET
+export const addWallet = (walletData) =>
+    makeRequest("post", "/wallet", walletData);
+
+export const deleteWallet = (walletId) =>
+    makeRequest("delete", `/wallet/${walletId}`);
+
+export const getUserWallets = () => makeRequest("get", "/wallet");
+
+export const updateWallet = (walletId, walletData) =>
+    makeRequest("put", `/wallet/${walletId}`, walletData);
+
+export const getWallet = (walletId) =>
+    makeRequest("get", `/wallet/${walletId}`);
+
+export const addBudget = (walletId, budgetData) =>
+    makeRequest("post", `/wallet/${walletId}/budget`, budgetData);
+
+export const deleteBudget = (walletId) =>
+    makeRequest("delete", `/wallet/${walletId}/budget`);
+
+export const getWalletSummary = (walletId) =>
+    makeRequest("get", `/wallet/${walletId}/summary`);
 
 export default apiClient;
