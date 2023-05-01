94ee6cd6e39f29a081c2f1fcad2fb62974eb96db
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-01 12:50:45 +0300

    mobile..added signout button to home settings

--- AiWallet_mobile/screens/home/HomeSettingsScreen.js
+++ AiWallet_mobile/screens/home/HomeSettingsScreen.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { ROUTES, FONTS, COLORS, SIZES } from "../../constants";
+import { Button } from "../../components";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function HomeSettings() {
+export default function HomeSettings({ navigation }) {
+    const handleSignOut = async () => {
+        try {
+            await AsyncStorage.removeItem("token");
+            navigation.reset({
+                index: 0,
+                routes: [{ name: ROUTES.AUTH }],
+            });
+        } catch (error) {
+            console.error("Error during sign out:", error);
+        }
+    };
     return (
         <View>
-            <Text>Home Screen Settings</Text>
+            <Button title="Sign Out" onPress={handleSignOut} />
         </View>
     );
 }
