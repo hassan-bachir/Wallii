330d4beb42ac56d288ff73b42d37c0f7afbe36f1
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-03 14:34:01 +0300

    mobile..added basic strucute to transaction screen

--- AiWallet_mobile/screens/wallet/TransactionScreen.js
+++ AiWallet_mobile/screens/wallet/TransactionScreen.js
@@ -5,4 +5,47 @@ import { Ionicons } from "@expo/vector-icons";
 
 const TransactionScreen = ({ route, navigation }) => {
     const { transaction } = route.params;
+    const [transactionTypeIndex, setTransactionTypeIndex] = useState(0);
+    const [amount, setAmount] = useState("");
+
+    const transactionTypes = ["Expense", "Income"];
+
+    const updateTransactionType = (selectedIndex) => {
+        setTransactionTypeIndex(selectedIndex);
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Ionicons
+                name="arrow-back"
+                size={24}
+                onPress={() => navigation.goBack()}
+                style={styles.backIcon}
+            />
+            <ButtonGroup
+                onPress={updateTransactionType}
+                selectedIndex={transactionTypeIndex}
+                buttons={transactionTypes}
+            />
+            <TextInput
+                style={styles.amountInput}
+                keyboardType="numeric"
+                onChangeText={setAmount}
+                value={amount}
+                placeholder="Amount"
+            />
+        </SafeAreaView>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    backIcon: {
+        margin: 10,
+    },
+    amountInput: {},
+});
+
+export default TransactionScreen;
