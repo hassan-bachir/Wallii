9b65ef72884401eed78c876db34aaf916e1d4050
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-05 01:34:38 +0300

    mobile: updated bottom tab

--- AiWallet_mobile/routes/WalletBottomTab.js
+++ AiWallet_mobile/routes/WalletBottomTab.js
@@ -2,5 +2,5 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import WalletScreen from "../screens/wallet/WalletScreen";
 import WalletBudget from "../screens/wallet/WalletBudgetScreen";
-import WalletScreen from "../screens/wallet/WalletStatsScreen";
-import WalletScreen from "../screens/wallet/WalletSettingsScreen";
+import WalletStats from "../screens/wallet/WalletStatsScreen";
+import WalletSettings from "../screens/wallet/WalletSettingsScreen";
