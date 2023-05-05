de4b19d6fd1e8f68b4a5f927a4d7ff84864ab705
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-05 12:33:09 +0300

    mobile: adde layouts to screens

--- AiWallet_mobile/screens/wallet/WalletStatsScreen.js
+++ AiWallet_mobile/screens/wallet/WalletStatsScreen.js
@@ -1,11 +1,51 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+    Background,
+    WalletCard,
+    AddWalletButton,
+    TransactionCard,
+    Container,
+} from "../../components";
+import { COLORS, IMAGES, ROUTES } from "../../constants";
+import { useFocusEffect } from "@react-navigation/native";
+import { getWalletSummary } from "../../api/api";
+import { useSelector } from "react-redux";
 
 const WalletStats = () => {
+    const walletId = useSelector((state) => state.wallet.currentWalletId);
+    const [wallet, setWallet] = useState(null);
+    const loadData = async () => {
+        try {
+            const fetchedWallet = await getWalletSummary(walletId);
+            setWallet(fetchedWallet);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    useFocusEffect(
+        React.useCallback(() => {
+            loadData();
+            return () => {};
+        }, [walletId])
+    );
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>test</Text>
-        </View>
+        <Background image={IMAGES.HOMEBACKGROUND}>
+            <View style={styles.walletCard}>
+                {wallet && (
+                    <WalletCard
+                        walletId={wallet._id}
+                        name={wallet.name}
+                        totalIncome={wallet.totalIncome}
+                        totalExpenses={wallet.totalExpenses}
+                    />
+                )}
+            </View>
+            <View style={styles.container}>
+                <Text style={styles.text}>test</Text>
+            </View>
+        </Background>
     );
 };
 
@@ -18,6 +58,10 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 24,
         fontWeight: "bold",
+    },
+    walletCard: {
+        marginHorizontal: 10,
+        marginTop: 5,
     },
 });
 
