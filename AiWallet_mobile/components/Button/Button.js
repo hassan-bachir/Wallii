978b6ddf116f1b0ad8e52e03f57a24c116714a9f
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-06 02:25:36 +0300

    mobile: added settings

--- AiWallet_mobile/components/Button/Button.js
+++ AiWallet_mobile/components/Button/Button.js
@@ -2,21 +2,27 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { COLORS, SIZES, FONTS } from "../../constants";
 
-const Button = ({ onPress, title, style, disabled = false }) => {
-    const backgroundColor = disabled ? COLORS.gray : COLORS.secondary;
+const Button = ({
+    onPress,
+    title,
+    style,
+    disabled = false,
+    backgroundColor = COLORS.secondary,
+}) => {
+    const buttonBackgroundColor = disabled ? COLORS.gray : backgroundColor;
     return (
         <View style={{ margin: SIZES.padding * 1 }}>
             <TouchableOpacity
                 style={{
                     height: 45,
-                    backgroundColor: backgroundColor,
+                    backgroundColor: buttonBackgroundColor,
                     borderRadius: SIZES.radius / 1.5,
                     alignItems: "center",
                     justifyContent: "center",
                     ...style,
                 }}
-                onPress={disabled ? null : onPress} // Disable onPress action when disabled
-                disabled={disabled} // Add the disabled prop to TouchableOpacity
+                onPress={disabled ? null : onPress}
+                disabled={disabled}
             >
                 <Text style={{ color: COLORS.white, ...FONTS.body3 }}>
                     {title}
--- AiWallet_mobile/screens/home/HomeSettingsScreen.js
+++ AiWallet_mobile/screens/home/HomeSettingsScreen.js
@@ -1,10 +1,39 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { ROUTES, FONTS, COLORS, SIZES } from "../../constants";
-import { Button } from "../../components";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import { ROUTES, FONTS, COLORS, SIZES, IMAGES } from "../../constants";
+import { Button, Background } from "../../components";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { updateUserInfo, getUserInfo } from "../../api/api";
 
 export default function HomeSettings({ navigation }) {
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [aiAdvisorName, setAiAdvisorName] = useState("");
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const userInfo = await getUserInfo();
+                setName(userInfo.name || "");
+                setLastName(userInfo.lastName || "");
+                setAiAdvisorName(userInfo.aiAdvisorName || "");
+            } catch (error) {
+                console.error("Error fetching user info:", error);
+            }
+        };
+        fetchUserInfo();
+    }, []);
+
+    const handleUpdateUser = async () => {
+        try {
+            const updatedUserData = { name, lastName, aiAdvisorName };
+            await updateUserInfo(updatedUserData);
+            navigation.goBack();
+        } catch (error) {
+            console.error("Error updating user:", error);
+        }
+    };
+
     const handleSignOut = async () => {
         try {
             await AsyncStorage.removeItem("token");
@@ -17,8 +46,68 @@ export default function HomeSettings({ navigation }) {
         }
     };
     return (
-        <View>
+        <View style={styles.container}>
+            <Text style={styles.title}>Update User Info</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>Name</Text>
+                <TextInput
+                    style={styles.input}
+                    value={name}
+                    onChangeText={setName}
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>Last Name</Text>
+                <TextInput
+                    style={styles.input}
+                    value={lastName}
+                    onChangeText={setLastName}
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>AI Advisor Name</Text>
+                <TextInput
+                    style={styles.input}
+                    value={aiAdvisorName}
+                    onChangeText={setAiAdvisorName}
+                />
+            </View>
+            <Button
+                title="Update User"
+                onPress={handleUpdateUser}
+                backgroundColor={COLORS.primary}
+            />
             <Button title="Sign Out" onPress={handleSignOut} />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: SIZES.padding,
+    },
+    title: {
+        ...FONTS.h3,
+        textAlign: "center",
+        marginTop: 20,
+        marginBottom: 10,
+        color: COLORS.primary,
+    },
+    inputContainer: {
+        marginBottom: 20,
+    },
+    label: {
+        ...FONTS.body2,
+        color: COLORS.primary,
+        marginBottom: 5,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: COLORS.primary,
+        borderRadius: 5,
+        padding: 10,
+        color: COLORS.primary,
+        ...FONTS.body3,
+    },
+});
