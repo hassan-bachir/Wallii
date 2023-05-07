457256c75306af9b3a9550db742ef9f5f6e4c620
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-07 13:17:53 +0300

    admin: reverted the app page

--- wallii_admin/src/App.js
+++ wallii_admin/src/App.js
@@ -1,8 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-    return <text>hello</text>;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
