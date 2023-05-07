7e3190fd2ffa113d3a02180032a9fecfba2bf015
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-07 19:00:11 +0300

    server: deleted not needed middleware

--- Wallii_server/middlewares/admin.middleware.js
+++
@@ -1,6 +0,0 @@
-exports.adminMiddleware = async (req, res, next) => {
-    // req.user.role ["user", "admin"]
-    if (req.user.role === "admin") return next();
-
-    return res.status(401).json({ message: "Unauthorized" });
-};
