b35ba1bfea444750a2c6ef285c9a99e34bfe799d
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-05-09 14:41:56 +0300

    mobile: added currency formatting

--- AiWallet_mobile/components/FinancialSummaryBanner/FinanceSummaryBanner.js
+++ AiWallet_mobile/components/FinancialSummaryBanner/FinanceSummaryBanner.js
@@ -7,27 +7,29 @@ const FinanceSummaryBanner = ({ financialSummary }) => {
     if (!financialSummary) {
         return null;
     }
-
+    const numberWithCommas = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
     return (
         <View style={styles.container}>
             <View style={styles.labelContainer}>
                 <Text style={styles.labelTextIncome}>Income</Text>
                 <Text style={styles.numberText}>
-                    ${financialSummary.totalIncome}
+                    ${numberWithCommas(financialSummary.totalIncome)}
                 </Text>
             </View>
             <View style={styles.separator} />
             <View style={styles.labelContainer}>
                 <Text style={styles.labelText}>Total</Text>
                 <Text style={styles.numberText}>
-                    ${financialSummary.difference}
+                    ${numberWithCommas(financialSummary.difference)}
                 </Text>
             </View>
             <View style={styles.separator} />
             <View style={styles.labelContainer}>
                 <Text style={styles.labelTextExpense}>Expenses</Text>
                 <Text style={styles.numberText}>
-                    ${financialSummary.totalExpenses}
+                    ${numberWithCommas(financialSummary.totalExpenses)}
                 </Text>
             </View>
         </View>
