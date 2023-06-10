e7925e796aa60ecff4cc1a3bf63fa710956d1537
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-06-10 13:03:56 +0300

    Halellujah

--- AiWallet_mobile/api/api.js
+++ AiWallet_mobile/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-const API_BASE_URL = "http://192.168.1.4:3000";
+const API_BASE_URL = "http://192.168.0.109:3000";
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
